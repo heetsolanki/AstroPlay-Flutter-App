7a36957b012eb0a95d802edc37ee1b00651af9f4
Heet Solanki <heetsolanki2006@gmail.com>
2025-09-14 09:04:51 +0530

    Create exports.dart

---
+++ astroplay/lib/exports.dart
@@ -0,0 +1,8 @@
+// Packages
+export 'package:flutter/material.dart';
+export 'package:google_fonts/google_fonts.dart';
+export 'package:material_symbols_icons/symbols.dart';
+
+// Files
+export 'main.dart';
+export 'screens/home_screen.dart';
