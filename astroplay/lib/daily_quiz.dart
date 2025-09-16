8d949589f8b23e3290804e142206272fabe4cd13
Heet Solanki <heetsolanki2006@gmail.com>
2025-09-16 19:05:19 +0530

    Create daily_quiz.dart

---
+++ astroplay/lib/daily_quiz.dart
@@ -0,0 +1,7 @@
+final List<Map<String, dynamic>> dailyQuestions = [
+  {
+    'question': 'On which planet do we live?',
+    'options': ['Mars', 'Uranus', 'Earth', 'Jupiter'],
+    'correctAnswer': 'Earth'
+  },
+];
